e3e2133506117c220ba7c54dbd746bc3df5e7998
Dmitry Shechtman <damnian@gmail.com>
2017-04-19 20:23:49 +0300

    Add solution

---
+++ src/Net.Chdk.Detectors.Card/CardDetector.cs
@@ -0,0 +1,55 @@
+﻿using Net.Chdk.Model.Card;
+using System.Linq;
+using System.Management;
+
+namespace Net.Chdk.Detectors.Card
+{
+    public sealed class CardDetector
+    {
+        private const string CardsQueryString = "SELECT * FROM Win32_Volume WHERE DriveType = 2";
+
+        public CardInfo[] GetCards()
+        {
+            using (var searcher = new ManagementObjectSearcher(CardsQueryString))
+            using (var volumes = searcher.Get())
+            {
+                return volumes
+                    .Cast<ManagementObject>()
+                    .Select(GetCard)
+                    .ToArray();
+            }
+        }
+
+        public CardInfo GetCard(string driveLetter)
+        {
+            using (var searcher = new ManagementObjectSearcher($"{CardsQueryString} AND DriveLetter = '{driveLetter}'"))
+            using (var volumes = searcher.Get())
+            {
+                return volumes
+                    .Cast<ManagementObject>()
+                    .Select(GetCard)
+                    .SingleOrDefault();
+            }
+        }
+
+        private CardInfo GetCard(ManagementObject volume)
+        {
+            var deviceId = (string)volume["DeviceID"];
+            var driveLetter = (string)volume["DriveLetter"];
+            var label = (string)volume["Label"];
+            var fileSystem = (string)volume["FileSystem"];
+            var capacity = (ulong)volume["Capacity"];
+            var freeSpace = (ulong)volume["FreeSpace"];
+
+            return new CardInfo
+            {
+                DeviceId = deviceId,
+                DriveLetter = driveLetter,
+                Label = label,
+                FileSystem = fileSystem,
+                Capacity = capacity,
+                FreeSpace = freeSpace,
+            };
+        }
+    }
+}
